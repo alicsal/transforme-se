115473e0cfd3b35ae9bbf7b874239fbe5c6d9244
Larissa Priscila <104330439+alicsal@users.noreply.github.com>
2025-11-25 16:55:36 -0300

    Add files via upload

---
+++ Larissa Priscila/aula4/exemplo3.js
@@ -0,0 +1,16 @@
+const input = require("../input");
+
+(()=>{
+
+    for(let i=0; i<10; i+=2){ //usado sempre qdo o problema envolve contagem 
+        
+        console.log("I: "+i)
+        let aleatorio = Math.floor(Math.random()*10);
+        console.log("Ale: "+aleatorio);
+    } 
+
+
+})()
+
+
+
