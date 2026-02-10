ee3fd5f801591723e7bd313c4edf50a72824d7c4
Larissa Priscila <104330439+alicsal@users.noreply.github.com>
2026-02-10 15:28:13 -0300

    Add files via upload

---
+++ SERASA/script.js
@@ -0,0 +1,6 @@
+// a variavel do tipo constante chamada "menuContact" recebe do documento cujo o id é contact
+const menuContact = document.getElementById("contact");
+
+menuContact.addEventListener("click", ()=>{
+    alert("Não trabalhamos com contato");
+});
